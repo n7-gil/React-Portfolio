cb220dfd11746f6d6c29993585102f868550555b
N7 GIL <gilbertoaescobedo@gmail.com>
2022-10-04 13:59:08 -0700

    edited App.js. created navbar and footer

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li class="footer-item"><a href="mailto:gilbertoaescobedo@gmail.com">Email</a></li>
+                <li class="footer-item"><a href="assets/images/RESUME.PNG">Resume</a></li>
+                <li class="footer-item"><a href="https://github.com/n7-gil" target="_blank" rel="noreferrer">Github</a></li>
+            </ul>
+            <h3>
+                Designed and made by Gilberto Escobedo &copy; 2022 N7-Gil
+            </h3>
+        </footer>
+    )
+}
---
+++ react-portfolio/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <header>
+            <h1>Gilberto Escobedo</h1>
+            <nav>
+                <ul>
+                    <li class="nav-item"><a href="#home-container">Home</a></li>
+                    <li class="nav-item"><a href="#about-me">About Me</a></li>
+                    <li class="nav-item"><a href="#work">Work</a></li>
+                    <li class="nav-item"><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
